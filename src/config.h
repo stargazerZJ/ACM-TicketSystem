f8a581fea82684c452a4b28812fbbd33c78677fe
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2024-05-18 00:06:47 +0800

    maximum buffer pool size

--- src/config.h
+++ src/config.h
@@ -25,7 +25,7 @@ static constexpr int VLS_PAGES_PER_FRAME = 1;
 using hash_t = uint64_t;
 
 static constexpr int LRU_REPLACER_K = 10;
-static constexpr int BUFFER_POOL_SIZE = 2500;
+static constexpr int BUFFER_POOL_SIZE = 8000;
 
 static constexpr char DB_FILE_NAME[] = "db.bin";
 
